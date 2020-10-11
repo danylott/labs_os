7cdeb784a932b0256566c13e0c4d6a5e5d3cccf5
danylott <d.tuytuyshkin@gmail.com>
2020-10-11 22:02:13 +0300

    move baseCommand to local function

--- lab1/src/com/company/Manager.java
+++ lab1/src/com/company/Manager.java
@@ -12,7 +12,6 @@ public class Manager {
     private final int multiplier;
     private final int delay;
     private Process process = null;
-    private final String baseCommand = "/home/neknu/.jdks/openjdk-15/bin/java -javaagent:/home/neknu/JetBrains/ideaIU-2020.2.2/idea-IU-202.7319.50/lib/idea_rt.jar=45241:/home/neknu/JetBrains/ideaIU-2020.2.2/idea-IU-202.7319.50/bin -Dfile.encoding=UTF-8 -classpath /home/neknu/University/3course/OS/lab1/out/production/lab1 com.company.Runner";
 
     public Manager(String name, int value, int multiplier, int delay) {
         this.name = name;
@@ -24,6 +23,7 @@ public class Manager {
     public void runCommand(String path_to_pipe) {
         try
         {
+            String baseCommand = "/home/neknu/.jdks/openjdk-15/bin/java -javaagent:/home/neknu/JetBrains/ideaIU-2020.2.2/idea-IU-202.7319.50/lib/idea_rt.jar=45241:/home/neknu/JetBrains/ideaIU-2020.2.2/idea-IU-202.7319.50/bin -Dfile.encoding=UTF-8 -classpath /home/neknu/University/3course/OS/lab1/out/production/lab1 com.company.Runner";
             String command = baseCommand + " " + path_to_pipe + " " + value + " " + multiplier + " " + delay + " " + name;
             var processBuilder = new ProcessBuilder();
 
